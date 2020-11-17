3e98778394b90eb7ee46d15bc44b1e3791f0fbd3
Racer <reaganjoseph26@yahoo.com>
2020-11-16 19:14:37 -0500

    fetching user repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,14 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-  response.json().then(function(data) {
-      console.log(data);
-  });
-});
-}
+var getUserRepos = function(user) {
 
-console.log("outside");
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos"
 
+    //make a request to the url 
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
+};
 
-getUserRepos()
+getUserRepos("facebook")
